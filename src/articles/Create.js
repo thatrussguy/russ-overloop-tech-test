f8da94f6dcc69745f3712c4d5a06a5a16254a50d
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2020-04-04 14:39:35 +0100

    update forms to handle territories

--- src/articles/Create.js
+++ src/articles/Create.js
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
-import {
-  withRouter,
-  Link,
-} from 'react-router-dom';
+import React, { Component } from "react";
+import { withRouter, Link } from "react-router-dom";
 import {
   Button,
   Col,
@@ -11,75 +8,95 @@ import {
   FormGroup,
   Input,
   Label,
-} from 'reactstrap';
+} from "reactstrap";
 
 class Create extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
-      title: '',
-      content: '',
+      title: "",
+      content: "",
+      territories: "",
       error: null,
       errors: {},
     };
   }
 
   updateTitle(ev) {
-    if(this.state.loading) return;
+    if (this.state.loading) return;
     this.setState({
       title: ev.target.value,
     });
   }
 
   updateContent(ev) {
-    if(this.state.loading) return;
+    if (this.state.loading) return;
     this.setState({
       content: ev.target.value,
     });
   }
 
-  submit() {
+  updateTerritories(ev) {
+    if (this.state.loading) return;
     this.setState({
-      loading: true,
-    }, async () => {
-      let result = await fetch('/api/articles/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          title: this.state.title,
-          content: this.state.content,
-        }),
-      });
-      if(result.status !== 200) {
-        this.setState({
-          loading: false,
-          error: await result.text(),
-        });
-        return;
-      }
-      let json = await result.json();
-      if(json.success) {
-        this.props.history.push('/');
-      } else {
-        this.setState({
-          loading: false,
-          errors: json.errors,
-        });
-      }
+      territories: ev.target.value,
     });
   }
 
+  submit() {
+    this.setState(
+      {
+        loading: true,
+      },
+      async () => {
+        let result = await fetch("/api/articles/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: this.state.title,
+            content: this.state.content,
+            // split territories by whitespace and map to { name: territory }
+            territories: this.state.territories
+              .replace(/  +/g, " ")
+              .split(" ")
+              .map((territory) => ({
+                name: territory,
+              })),
+          }),
+        });
+        if (result.status !== 200) {
+          this.setState({
+            loading: false,
+            error: await result.text(),
+          });
+          return;
+        }
+        let json = await result.json();
+        if (json.success) {
+          this.props.history.push("/");
+        } else {
+          this.setState({
+            loading: false,
+            errors: json.errors,
+          });
+        }
+      }
+    );
+  }
+
   render() {
-    if(this.state.error) {
+    if (this.state.error) {
       return <div>{this.state.error.toString()}</div>;
     }
     return (
       <Form>
         <FormGroup row>
-          <Label for="title" sm={2}>Title</Label>
+          <Label for="title" sm={2}>
+            Title
+          </Label>
           <Col sm={10}>
             <Input
               type="text"
@@ -87,33 +104,58 @@ class Create extends Component {
               id="title"
               placeholder="Title"
               sm={10}
-              onChange={e => this.updateTitle(e)}
+              onChange={(e) => this.updateTitle(e)}
               value={this.state.title}
               disabled={this.state.loading}
-              invalid={this.state.errors.title} />
-            {
-              this.state.errors.title ?
-                <FormFeedback>{this.state.errors.title.message}</FormFeedback>
-              : ''}
+              invalid={Boolean(this.state.errors.title)}
+            />
+            {this.state.errors.title ? (
+              <FormFeedback>{this.state.errors.title.message}</FormFeedback>
+            ) : (
+              ""
+            )}
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label for="content" sm={2}>Content</Label>
+          <Label for="content" sm={2}>
+            Content
+          </Label>
           <Col sm={10}>
             <Input
               type="textarea"
               name="content"
               id="content"
               placeholder="Article Content"
-              onChange={e => this.updateContent(e)}
+              onChange={(e) => this.updateContent(e)}
               value={this.state.content}
-              disabled={this.state.loading} />
+              disabled={this.state.loading}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="territories" sm={2}>
+            Territories
+          </Label>
+          <Col sm={10}>
+            <Input
+              type="textarea"
+              name="territories"
+              id="territories"
+              placeholder="Article Territories"
+              onChange={(e) => this.updateTerritories(e)}
+              value={this.state.territories}
+              disabled={this.state.loading}
+            />
           </Col>
         </FormGroup>
         <FormGroup check row>
           <Col sm={{ size: 10, offset: 2 }}>
-            <Button onClick={() => this.submit()}  disabled={this.state.loading}>Submit</Button>
-            <Button tag={Link} to='/' disabled={this.state.loading}>Cancel</Button>
+            <Button onClick={() => this.submit()} disabled={this.state.loading}>
+              Submit
+            </Button>
+            <Button tag={Link} to="/" disabled={this.state.loading}>
+              Cancel
+            </Button>
           </Col>
         </FormGroup>
       </Form>
--- src/articles/Single.js
+++ src/articles/Single.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-import {
-  Link,
-} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import {
   Button,
   Col,
@@ -10,7 +8,7 @@ import {
   FormGroup,
   Input,
   Label,
-} from 'reactstrap';
+} from "reactstrap";
 
 class Single extends Component {
   constructor(props) {
@@ -18,36 +16,49 @@ class Single extends Component {
     this.state = {
       loading: false,
       error: null,
-      title: '',
-      content: '',
+      title: "",
+      content: "",
+      territories: "",
       errors: {},
     };
   }
 
   updateTitle(ev) {
-    if(this.state.loading) return;
+    if (this.state.loading) return;
     this.setState({
       title: ev.target.value,
     });
   }
 
   updateContent(ev) {
-    if(this.state.loading) return;
+    if (this.state.loading) return;
     this.setState({
       content: ev.target.value,
     });
   }
 
+  updateTerritories(ev) {
+    if (this.state.loading) return;
+    this.setState({
+      territories: ev.target.value,
+    });
+  }
+
   async fetch(id) {
-    if(!id) {
+    if (!id) {
       id = this.props.match.params.articleId;
     }
     try {
-      await new Promise(res => this.setState({
-        loading: true,
-      }, res));
+      await new Promise((res) =>
+        this.setState(
+          {
+            loading: true,
+          },
+          res
+        )
+      );
       let result = await fetch(`/api/articles/${id}/`);
-      if(result.status !== 200) {
+      if (result.status !== 200) {
         this.setState({
           loading: false,
           error: await result.text(),
@@ -61,8 +72,9 @@ class Single extends Component {
         id: json.article._id,
         title: json.article.title,
         content: json.article.content,
+        territories: json.article.territories.map(({ name }) => name).join(" "),
       });
-    } catch(e) {
+    } catch (e) {
       this.setState({
         loading: false,
         error: e,
@@ -71,57 +83,68 @@ class Single extends Component {
   }
 
   submit() {
-    this.setState({
-      loading: true,
-    }, async () => {
-      let result = await fetch(`/api/articles/${this.state.id}/`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          title: this.state.title,
-          content: this.state.content,
-        }),
-      });
-      if(result.status !== 200) {
-        this.setState({
-          loading: false,
-          error: await result.text(),
+    this.setState(
+      {
+        loading: true,
+      },
+      async () => {
+        let result = await fetch(`/api/articles/${this.state.id}/`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: this.state.title,
+            content: this.state.content,
+            // split territories by whitespace and map to { name: territory }
+            territories: this.state.territories
+              .replace(/  +/g, " ")
+              .split(" ")
+              .map((territory) => ({
+                name: territory,
+              })),
+          }),
         });
-        return;
+        if (result.status !== 200) {
+          this.setState({
+            loading: false,
+            error: await result.text(),
+          });
+          return;
+        }
+        let json = await result.json();
+        if (json.success) {
+          this.props.history.push("/");
+        } else {
+          this.setState({
+            loading: false,
+            errors: json.errors,
+          });
+        }
       }
-      let json = await result.json();
-      if(json.success) {
-        this.props.history.push('/');
-      } else {
-        this.setState({
-          loading: false,
-          errors: json.errors,
-        });
-      }
-    });
+    );
   }
-
 
   componentWillMount() {
     this.fetch();
   }
 
   componentWillReceiveProps(newProps) {
-    if(this.props.match.params.articleId !== newProps.match.params.articleId) {
+    if (this.props.match.params.articleId !== newProps.match.params.articleId) {
       this.fetch(newProps.match.params.articleId);
     }
   }
 
   render() {
-    if(this.state.error) {
+    if (this.state.error) {
       return <div>{this.state.error.toString()}</div>;
     }
     return (
       <Form>
         <FormGroup row>
-          <Label for="title" sm={2}>Title</Label>
+          <Label for="title" sm={2}>
+            Title
+          </Label>
           <Col sm={10}>
             <Input
               type="text"
@@ -129,33 +152,58 @@ class Single extends Component {
               id="title"
               placeholder="Title"
               sm={10}
-              onChange={e => this.updateTitle(e)}
+              onChange={(e) => this.updateTitle(e)}
               value={this.state.title}
               disabled={this.state.loading}
-              invalid={this.state.errors.title} />
-            {
-              this.state.errors.title ?
-                <FormFeedback>{this.state.errors.title.message}</FormFeedback>
-              : ''}
+              invalid={this.state.errors.title}
+            />
+            {this.state.errors.title ? (
+              <FormFeedback>{this.state.errors.title.message}</FormFeedback>
+            ) : (
+              ""
+            )}
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label for="content" sm={2}>Content</Label>
+          <Label for="content" sm={2}>
+            Content
+          </Label>
           <Col sm={10}>
             <Input
               type="textarea"
               name="content"
               id="content"
               placeholder="Article Content"
-              onChange={e => this.updateContent(e)}
+              onChange={(e) => this.updateContent(e)}
               value={this.state.content}
-              disabled={this.state.loading} />
+              disabled={this.state.loading}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="territories" sm={2}>
+            Territories
+          </Label>
+          <Col sm={10}>
+            <Input
+              type="textarea"
+              name="territories"
+              id="territories"
+              placeholder="Article Territories"
+              onChange={(e) => this.updateTerritories(e)}
+              value={this.state.territories}
+              disabled={this.state.loading}
+            />
           </Col>
         </FormGroup>
         <FormGroup check row>
           <Col sm={{ size: 10, offset: 2 }}>
-            <Button onClick={() => this.submit()}  disabled={this.state.loading}>Submit</Button>
-            <Button tag={Link} to='/' disabled={this.state.loading}>Cancel</Button>
+            <Button onClick={() => this.submit()} disabled={this.state.loading}>
+              Submit
+            </Button>
+            <Button tag={Link} to="/" disabled={this.state.loading}>
+              Cancel
+            </Button>
           </Col>
         </FormGroup>
       </Form>
